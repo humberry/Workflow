0d7f76dca62c0b18420e24f812888413692f2795
humberry <humberry@users.noreply.github.com>
2015-01-19 18:46:29 +0100

    Update workflow-calls-pythonista-with-argv.py

--- workflow-calls-pythonista-with-argv.py
+++ workflow-calls-pythonista-with-argv.py
@@ -1,5 +1,5 @@
 #Workflow:
-#Text (pythonista://my-py-script?action=run&argv=two parameter&argv="one parameter with spaces")
+#Text (pythonista://my-py-script?action=run&argv=two parameters&argv="one parameter with spaces")
 #Copy to Clipboard
 #URL (URL Clipboard)
 #Open URL
