b1c255f4ea965d2ffbf06e2f84f4a9283cafc1a0
humberry <humberry@users.noreply.github.com>
2015-01-19 18:41:09 +0100

    Create workflow-calls-pythonista-and-get-back.py

---
+++ workflow-calls-pythonista-and-get-back.py
@@ -0,0 +1,9 @@
+#Workflow:
+#Text (pythonista://my-py-script?action=run)
+#Copy to Clipboard
+#URL (URL Clipboard)
+#Open URL
+#Wait to Return
+#Show Alert (Titel: Alert Welcome back)
+
+print 'Pythonista action'
