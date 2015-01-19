210920913b155426c6004bd02651574ee6c1a340
humberry <humberry@users.noreply.github.com>
2015-01-19 18:41:36 +0100

    Create workflow-calls-pythonista-with-argv.py

---
+++ workflow-calls-pythonista-with-argv.py
@@ -0,0 +1,8 @@
+#Workflow:
+#Text (pythonista://my-py-script?action=run&argv=two parameter&argv="one parameter with spaces")
+#Copy to Clipboard
+#URL (URL Clipboard)
+#Open URL
+
+import sys
+print(sys.argv)
